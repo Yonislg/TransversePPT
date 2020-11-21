2d9cb11d3b354a957ca8d9968cd1b9e36e9b1068
Yonislg <yslegrand1@gmail.com>
2020-11-21 11:49:16 +0100

    iterative loop for emission comparisons

--- +transversemodel/+testfolder/emissions_comparisonsV3.m
+++ +transversemodel/+testfolder/emissions_comparisonsV3.m
@@ -8,7 +8,7 @@ import transversemodel.subfunctions.*;
 
 % General Parametrs
 e = 1.602176634e-19;
-m_i = 1.67262192369e-27;
+pre = [1 12 12 19 19 19]
 
 % Test FEE
 W = 4.0; %Workfunction 
@@ -20,14 +20,14 @@ A_G = 1.2016e+06/2;
 Twk = 1000;   %Kelvin 
 
 % Test SEE
-E_i = 13.6;                    % Ionization energy of hydrogen in electronvolt [eV]
+E_i = [13.6 11.3 24.4 47.9 17.4 35 62.7] ;                    % Ionization energy of hydrogen in electronvolt [eV]
 
 
 % Main iterables
-iter= 900;
+iter= 200;
 Te = 3*e;
 %n =  linspace(10^5,3*10^23,iter);       % Electron density  [m^-3]
-n =  logspace(6,24,iter);       % Electron density  [m^-3]
+n =  logspace(1,25,iter);       % Electron density  [m^-3]
 varphi = linspace(-1,-2000,iter);
 [N,V] = meshgrid(n,varphi);
 
@@ -36,27 +36,32 @@ varphi = linspace(-1,-2000,iter);
 %E_w1 = linspace(0,40)*10^8; % Electric field
 
 % GET Inspiration from wall field!
+for i = 1:6
+m_i = pre(1)*1.67262192369e-27;
 
-[SEE_o, FEE_o, TEE_o, E] = iterfield(Twk, V, N, Te, A_G, E_i, W, E_F, CorF);
-
+[SEE_o, FEE_o, TEE_o, E] = iterfield(Twk, V, N, Te, A_G, E_i(i), W, E_F, CorF,m_i);
+%% Plot
 figure(1)
-plot(n,E')
+hold on
+loglog(n,max(E'))
 title("Density and Electric field")    
 figure(2)
-%semilogy(n,SEE_o,'-',n,TEE_o,':',n,FEE_o,'--')
-semilogy(n, min(SEE_o),'-',n,max(TEE_o),':',n,max(FEE_o),'--')
+hold on
+%loglog(n,SEE_o,'-',n,TEE_o,':',n,FEE_o,'--')% or use loglog
+loglog(n, min(SEE_o),'-',n,max(TEE_o),':',n,max(FEE_o),'--')
 ulm = ceil(log10(max(max([TEE_o,FEE_o,SEE_o]))));   % To set a boundary of interest for the plot
-ylim([10^(-20) 10^(ulm+5)])
+%ylim([10^(-10) 10^(ulm+5)])
 title(sprintf('Maximum Electron emissions (T_e = %u eV)',Te/e))
 xlabel('Density [m^{-3}]')
 ylabel('Current density [A/m^2]')
 legend("SEE","TEE","FEE")
 
+end
+%% Required functions
 
-function [SEE_o, FEE_o, TEE_o, E] = iterfield(Twk, varphi, n, Te, A_G, E_i, W, E_F, CorF)
+function [SEE_o, FEE_o, TEE_o, E] = iterfield(Twk, varphi, n, Te, A_G, E_i, W, E_F, CorF, m_i)
 import transversemodel.subfunctions.*;
     % general parameter
-    m_i = 1.67262192369e-27;
     e = 1.602176634e-19;
     
     % Ion velocity
@@ -65,23 +70,30 @@ import transversemodel.subfunctions.*;
     
     % Iterating over the E_w field
     E_w = wall_e_field(Twk, varphi, SEE(gi, E_i, W), n, Te);
+    %E = E_w;
     %E_w(imag(E_w)~=0) = nan;
-    ge =  schottky(Twk, W, E_w, A_G)+ SEE(gi, E_i, W) + FEE(W,E_F, E_w, CorF)
+    ge =  schottky(Twk, W, E_w, A_G)+ SEE(gi, E_i, W) + FEE(W,E_F, E_w, CorF);
     geT = schottky(Twk, W, E_w, A_G);
     geS = SEE(gi, E_i, W);
     geF = FEE(W,E_F, E_w, CorF);
-    ge(:,1:3)
-    geT(:,1:3)
+%     ge(:,1:3);
+%     geT(:,1:3);
     %geS(:,1:3)
     %geF(:,1:3)
      %schottky(Twk, W, E_w, A_G) 
      %FEE(W,E_F, E_w, CorF)
-    E_w = wall_e_field(Twk, varphi, ge, n, Te)
+     
+    E_w = wall_e_field(Twk, varphi, ge, n, Te);
+    
     %E_w(imag(E_w)~=0) = nan;
     ge =  schottky(Twk, W, E_w, A_G)+ SEE(gi, E_i, W) + FEE(W,E_F, E_w, CorF);
      %schottky(Twk, W, E_w, A_G)
      %FEE(W,E_F, E_w, CorF)
+     
+    %richtig: 
     E = wall_e_field(Twk, varphi, ge, n, Te);
+    
+    
     %E(imag(E)~=0) = nan;
     SEE_o = SEE(gi, E_i, W)*e;
     FEE_o = FEE(W,E_F, E_w, CorF)*e;
