f185552a0b5c5fe96b744adca2e0c4ffe5aadb97
Yonislg <yslegrand1@gmail.com>
2020-12-07 22:16:27 +0100

    Revert "Committing stuff to merge"
    This reverts commit 37192ddf163168ab088b02394374adf405764659.

--- +transversemodel/+transversemodel/+subfunctions/SEE.m
+++
@@ -1,9 +0,0 @@
-% Function calculates secondary electron emissions
-function ge = SEE(gi, E_i, W)
-    if E_i-W>W
-        gamma = 0.016 *(E_i - 2* W);
-        ge = gi .* gamma;
-    else
-        ge = 0;
-    end
-end
